09d33c53f81f0991e8c9e35b28d15e74dd488b00
Patrick Vieira <107551156+Adromane@users.noreply.github.com>
2025-02-16 15:03:33 +0100

    commit change

--- src/portfolio.js
+++ src/portfolio.js
@@ -340,7 +340,6 @@ const degrees = {
       title: "Université Catholique de l'Afrique de l'Ouest (UCAO)",
       subtitle: "LICENCE PROFESSIONNELLE EN INFORMATIQUE DE GESTION",
       logo_path: "Université-logo.png",
-      logo_path: "1622120545040-removebg-preview.png", 
       alt_name: "Institut Supérieur de Gestion Saint Michel",
       duration: "2021 - 2022",
       descriptions: [

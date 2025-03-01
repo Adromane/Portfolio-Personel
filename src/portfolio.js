e01ce077b1af377e966fd0710723e737642be4bb
Patrick Vieira <107551156+Adromane@users.noreply.github.com>
2025-03-01 23:00:45 +0100

    commit change

--- src/portfolio.js
+++ src/portfolio.js
@@ -397,7 +397,7 @@ const experience = {
           duration: "June 2023 - actuelment",
           location: "Dakar",  
           description:
-            "Sur mon GitHub, je publie mes réalisations en développement, allant d’applications web à des projets open-source. Vous y trouverez du code en [tes technologies principales] ainsi que des solutions innovantes et collaboratives. 🚀",
+            "Sur mon GitHub, je publie mes réalisations en développement, allant d’applications web à des projets open-source. Vous y trouverez du code en [tes technologies principales] ainsi que des solutions innovantes et collaboratives.",
           color: "#0879bf",
         },
         {

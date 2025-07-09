3bff3f937b971563166cb0e32e93470f2dc24d82
Patrick Vieira <107551156+Adromane@users.noreply.github.com>
2025-07-09 18:45:09 +0100

    commit push

--- src/portfolio.js
+++ src/portfolio.js
@@ -397,7 +397,7 @@ const experience = {
           duration: "June 2024 - actuelment",
           location: "Dakar",  
           description:
-            "Sur mon GitLab, je centralise mes projets techniques, incluant des applications web, des scripts automatisés et des solutions innovantes. Vous y trouverez du code en [tes technologies principales], avec un accent particulier sur la qualité, la collaboration et l’amélioration continue. Ce dépôt reflète mon engagement à partager des outils utiles et à explorer de nouvelles approches dans le développement logiciel.",
+            "Sur mon GitLab, je centralise mes projets techniques, incluant des applications web, des scripts automatisés et des solutions innovantes. Vous y trouverez du code en [tes technologies principales], avec un accent particulier sur la qualité, la collaboration et l’amélioration continue.",
           color: "#0879bf",
         },
         {

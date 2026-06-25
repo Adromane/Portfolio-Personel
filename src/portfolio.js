b08c79e006d4588e2d8bc0a89e23fdc52fc487dc
Patrick Vieira <107551156+Adromane@users.noreply.github.com>
2026-06-25 03:38:07 +0100

    commit change

--- src/portfolio.js
+++ src/portfolio.js
@@ -125,7 +125,7 @@ const skills = {
       fileName: "FullStackImg",
       skills: [
         "⚡ Creation d'un site Web réactif avec React-Redux",
-        "⚡ Développement d'applications mobiles à l'aide de Flutter, React Native et d'applications Android solo a l'aide de Kotlin",
+        "⚡ Developpement d'applications mobiles à l'aide de Flutter, React Native et d'applications Android solo a l'aide de Kotlin",
         "⚡Création du backend de l'application dans Node, Express & Flask",
       ],
       softwareSkills: [

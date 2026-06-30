5f4b95e9a35cb4dc5d36bd4b661185595b959b06
Patrick Vieira <107551156+Adromane@users.noreply.github.com>
2026-06-30 02:12:01 +0100

    commit push

--- src/portfolio.js
+++ src/portfolio.js
@@ -207,7 +207,7 @@ const skills = {
       fileName: "CloudInfraImg",
       skills: [
         "⚡Expérience de travail sur plusieurs plates-formes cloud",
-        "⚡ Hébergement et maintenance de sites Web sur des instances de machines virtuelles avec integration de bases de donnees",
+        "⚡Hébergement et maintenance de sites Web sur des instances de machines virtuelles avec integration de bases de donnees",
         "⚡ Déploiement de modeles d'apprentissage en profondeur sur le cloud a utiliser sur les appareils mobiles",
         "⚡ Mise en place de la base de donnees au serveur.",
       ],

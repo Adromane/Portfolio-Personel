a7616e42f45e89dfb19000173bcc0c4d37ff6ce4
Patrick Vieira <107551156+Adromane@users.noreply.github.com>
2026-07-02 06:14:41 +0100

    Commit push

--- src/portfolio.js
+++ src/portfolio.js
@@ -385,7 +385,7 @@ const experience = {
         },
         {
           title: "💻 Bienvenue sur ma page",
-          company: "Page facebook",
+          company: "Facebook",
           company_url: "https://www.facebook.com/profile.php?id=61566801371695",
           logo_path: "facebook_logo.png",
           duration: "June 2024 - actuelment",

edb96a0e5f2d3edc35c0104970dc146b8533fb84
Patrick Vieira <107551156+Adromane@users.noreply.github.com>
2026-07-02 05:46:30 +0100

    commit push

--- src/portfolio.js
+++ src/portfolio.js
@@ -376,7 +376,7 @@ const experience = {
           title: "📸 Suivez-moi sur Instagram",
           company: "Instagram",
           company_url: "https://github.com/Adromane",
-          logo_path: "social",
+          logo_path: "github_logo.png",
           duration: "June 2024 - actuelment",
           location: "Dakar",
           description:

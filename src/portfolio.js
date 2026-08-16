0a81604c33937ae433dbc73b63bc58f146fa9ee4
Patrick Vieira <107551156+Adromane@users.noreply.github.com>
2026-08-16 03:39:31 +0100

    Commit push

--- src/portfolio.js
+++ src/portfolio.js
@@ -430,7 +430,7 @@ const experience = {
       duration: "Juin 2026 - Actuellement",
       location: "Av. Amílcar Cabral, Porto de Bissau, Bissau, Guiné-Bissau",
       description:
-        "Dans le cadre de mon stage professionnel à l’Administration des Ports de Guinée-Bissau (APGB), j’intègre actuellement le Service de Contrôle et Fiscalisation, où je participe aux différentes activités liées au contrôle, au suivi et au traitement des informations relatives aux opérations portuaires..",
+        "Dans le cadre de mon stage professionnel à l’Administration des Ports de Guinée-Bissau (APGB), j’intègre actuellement le Service de Contrôle et Fiscalisation, où je participe aux différentes activités liées au contrôle, au suivi et au traitement des informations relatives aux opérations portuaires.",
       color: "#0071C5",
     },
   ],
